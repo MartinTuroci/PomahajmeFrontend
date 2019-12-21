d224b7fbf39e34a98bb07bca27509e591c9bab9e
Martin Turoci <martinturoci@gmail.com>
2019-12-21 13:13:34 +0100

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,24 @@
 # Install ncftp
 sudo apt-get install ncftp
 
-ncftp -u $USER -p $PASSWORD $HOST
-# Create tmp dir
-mkdir tmpDir
-
-# Remove old version while keeping permanent data
-rename www/images tmpDir/images
-rename www/auctionImages tmpDir/auctionImages
-rename www/robots.txt tmpDir/robots.txt
-rename www/web.config tmpDir/web.config
-rename www/.htaccess tmpDir/.htaccess
-rename www/subdom tmpDir/subdom
-rename www/domains tmpDir/domains
-
-rmdir www
-mkdir www
-
-rename tmpDir/images www/images
-rename tmpDir/auctionImages www/auctionImages
-rename tmpDir/robots.txt www/robots.txt
-rename tmpDir/web.config www/web.config
-rename tmpDir/.htaccess www/.htaccess
-rename tmpDir/subdom www/subdom
-rename tmpDir/domains www/domains
-
-rmdir tmpDir
-
-quit
-
 # Upload new version
-ncftpput -R -v -u $USER -p $PASSWORD $HOST /www /dist/.
-quit
+ncftpput -R -v -u $USER -p $PASSWORD \
+-W "mkdir tmpDir" \
+-W "rename www/images tmpDir/images" \
+-W "rename www/auctionImages tmpDir/auctionImages" \
+-W "rename www/robots.txt tmpDir/robots.txt" \
+-W "rename www/web.config tmpDir/web.config" \
+-W "rename www/.htaccess tmpDir/.htaccess" \
+-W "rename www/subdom tmpDir/subdom" \
+-W "rename www/domains tmpDir/domains" \
+-W "rmdir www" \
+-W "mkdir www" \
+-W "rename tmpDir/images www/images" \
+-W "rename tmpDir/auctionImages www/auctionImages" \
+-W "rename tmpDir/robots.txt www/robots.txt" \
+-W "rename tmpDir/web.config www/web.config" \
+-W "rename tmpDir/.htaccess www/.htaccess" \
+-W "rename tmpDir/subdom www/subdom" \
+-W "rename tmpDir/domains www/domains" \
+-W "rmdir tmpDir" \
+$HOST /www /dist/.
