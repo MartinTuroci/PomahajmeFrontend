ea2072b4c2e3a297eb7e437a4f4db9ac50d33e47
MartinT <martinturoci@gmail.com>
2020-12-30 21:35:02 +0100

    feat: add file number

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -6,7 +6,8 @@
       <h3>OZ Podaj Pomoc</h3>
       <p>Okružná 142</p>
       <p>08212 Kapušany</p>
-      <b>IČO:</b><span> 53358066</span>
+      <p><b>IČO:</b><span> 53358066</span></p>
+      <b>MV SR, číslo spisu:</b><span> VVS/1-900/90-60082</span>
       <p><i class="fas fa-envelope text-blue"></i> info@pomahajme.sk</p>
       <p><i class="fas fa-phone text-blue"></i> 0949 01 22 02</p>
     </article>
