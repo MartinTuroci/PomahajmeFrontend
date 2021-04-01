1c54228194affadccc985fe071aada8b0910288f
MartinT <martinturoci@gmail.com>
2021-04-01 19:42:31 +0200

    feat: add owner name to the contact page

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -8,6 +8,7 @@
       <p>08212 Kapušany</p>
       <p><b>IČO:</b><span> 53358066</span></p>
       <b>MV SR, číslo spisu:</b><span> VVS/1-900/90-60082</span>
+      <p><i class="fas fa-id-card text-blue"></i> Tomáš Berka zakladateľ a predseda o.z.</p>
       <p><i class="fas fa-envelope text-blue"></i> info@pomahajme.sk</p>
       <p><i class="fas fa-phone text-blue"></i> 0949 01 22 02</p>
     </article>
