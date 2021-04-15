fe8cde556d1c3615a5a82299bceb007b31512d25
MartinT <martinturoci@gmail.com>
2021-04-15 21:20:55 +0200

    chore(about-us): update desc

--- pages/o-nas.vue
+++ pages/o-nas.vue
@@ -3,18 +3,15 @@
     <base-header heading="O nás"></base-header>
     <article class="mt-3">
       <p>
-        Sme mladý tím ľudí, ktorí sa rozhodli, že vo svojom voľnom čase budú
-        pomáhať ľuďom, ktorí mali v živote menej štastia než my a sú odkázaní na
-        pomoc druhých.
+        Sme mladý tím ľudí, ktorí sa rozhodli, že vo svojom voľnom čase budú pomáhať ľuďom, ktorí mali v
+        živote menej štastia než my a sú odkázaní na pomoc druhých.
       </p>
       <br />
       <p>
-        Cieľom tohto projektu je teda poskytnúť pomoc (či už finančnú alebo
-        materiálnu napr. formou zaslania nepotrebných starých vecí).
-        Vierohodnosť je overovaná našim tímom. Každý príbeh zverejňuje svoj
-        vlastný účet a všetky poslané peniaze idú priamo im. Náš portál
-        nestrháva žiadne poplatky za poskytnutú pomoc ako to môže byť zvykom.
-        Veríme, že nezištná pomoc je tá jediná správna pomoc.
+        Cieľom tohto projektu je teda poskytnúť pomoc (či už finančnú alebo materiálnu napr. formou zaslania
+        nepotrebných starých vecí). Vierohodnosť je overovaná našim tímom. Náš portál nestrháva žiadne
+        poplatky za poskytnutú pomoc ako to môže byť zvykom. Veríme, že nezištná pomoc je tá jediná správna
+        pomoc.
       </p>
     </article>
   </section>
