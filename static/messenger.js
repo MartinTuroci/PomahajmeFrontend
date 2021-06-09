b28d9d755986128fc2eb8f7671cb838b22b82f8e
MartinT <martinturoci@gmail.com>
2021-06-09 21:39:58 +0200

    Revert "feat: fb messenger - slovak lang"
    This reverts commit 2399ea4b1fd48c3ce4cfd3d6867d8d1c0ac4d324.

--- static/messenger.js
+++ static/messenger.js
@@ -12,6 +12,6 @@ window.fbAsyncInit = function () {
     var js, fjs = d.getElementsByTagName(s)[0]
     if (d.getElementById(id)) return
     js = d.createElement(s); js.id = id
-    js.src = 'https://connect.facebook.net/sk_SK/sdk/xfbml.customerchat.js'
+    js.src = 'https://connect.facebook.net/cs_CZ/sdk/xfbml.customerchat.js'
     fjs.parentNode.insertBefore(js, fjs)
   }(document, 'script', 'facebook-jssdk'))
